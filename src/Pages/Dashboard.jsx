6e6df12b4909a3ab4b1f389ea39a87ef463e091c
Piyush Jaiswal <thejaiswal1@gmail.com>
2024-12-13 11:10:30 +0530

    Update Dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -5,9 +5,19 @@ import { Link } from "react-router-dom";
 const Dashboard = () => {
   return (
     <>
-      <div className="flex-1" >
-      <Link to='/dashboard' className="block py-3 pl-5 px-5 font-bold text-3xl Times text-center mx-auto">Dashboard</Link>
-        <StatisticsTabsMenu />
+      <div className="flex-1 bg-gray-500 bg-opacity-40 pb-6 backdrop-blur-md min-h-screen">
+        {/* Dashboard Link */}
+        <Link 
+          to='/dashboard' 
+          className="block py-3 px-3 font-bold text-3xl text-gray-700 Times text-center mx-auto"
+        >
+          Dashboard
+        </Link>
+
+        {/* StatisticsTabsMenu without 3D effect */}
+        <div className="mt-6 mx-6 shadow-xl rounded-lg p-6 bg-white">
+          <StatisticsTabsMenu />
+        </div>
       </div>
     </>
   );
