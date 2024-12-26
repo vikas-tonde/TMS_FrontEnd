a37b945bb341ebc550c2d90fb5193599a5e7036e
Piyush Jaiswal <thejaiswal1@gmail.com>
2024-12-26 12:31:18 +0530

    Updated UI and added validation UI components

--- src/Pages/BulkUserEntry.jsx
+++ src/Pages/BulkUserEntry.jsx
@@ -9,7 +9,7 @@ import { Link } from "react-router-dom";
 const bulkUserEntrySchema = object().shape({
   batchName: string().required("Batch Name is Required"),
   location: string().required("Location is required"),
-  userDetailsFile: mixed().required('required')
+  userDetailsFile: mixed().required('File is required')
 });
 
 function BulkEntryXlsx() {
@@ -27,7 +27,7 @@ function BulkEntryXlsx() {
       });
       toast.success('Users added...!');
     } catch (error) {
-      toast.error('Something went wrong while adding Users please check all fields carefully...!');
+      toast.error('Something went wrong while adding Users, please check all fields carefully...!');
       console.log(error.response);
     }
     actions.resetForm();
@@ -60,60 +60,45 @@ function BulkEntryXlsx() {
               <div className="space-y-6">
 
                 {/* Batch Name */}
-                <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-                  <label className="text-gray-700 text-xl font-bold mb-2 sm:mr-4">Batch Name</label>
-                  <input
-                    type="text"
-                    id="batchName"
-                    name="batchName"
-                    placeholder="Enter Batch Name"
-                    value={values.batchName}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className="shadow appearance-none block bg-white rounded-md w-full sm:w-96 h-9 py-2 px-3 ring-1 ring-inset ring-gray-400 focus:text-gray-800"
-                  />
+                <div className="flex flex-col sm:flex-row items-start justify-between">
+                  <label htmlFor="batchName" className="text-gray-700 text-xl font-bold mb-2 sm:mr-4">Batch Name</label>
+                  <div className="w-full sm:w-96">
+                    <input
+                      type="text"
+                      id="batchName"
+                      name="batchName"
+                      placeholder="Enter Batch Name"
+                      value={values.batchName}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      className="shadow appearance-none block bg-white rounded-md w-full h-9 py-2 px-3 ring-1 ring-inset ring-gray-400 focus:text-gray-800"
+                    />
+                    {errors.batchName && touched.batchName && <p className="text-[#dc2626]">{errors.batchName}</p>}
+                  </div>
                 </div>
 
                 {/* Location */}
-                {/* <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-                  <label className="text-gray-700 text-xl font-bold mb-2 sm:mr-4">Location</label>
-                  <input
-                    type="text"
-                    id="location"
-                    name="location"
-                    placeholder="Enter Location"
-                    value={values.location}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className="shadow appearance-none block bg-white rounded-md w-full sm:w-96 h-9 py-2 px-3 ring-1 ring-inset ring-gray-400 focus:text-gray-800"
-                  />
-                </div> */}
-
                 <div className="flex flex-col sm:flex-row items-start justify-between">
-                  <label htmlFor="batch" className="text-gray-700 text-xl font-bold mb-2 sm:mr-4">Location</label>
+                  <label htmlFor="location" className="text-gray-700 text-xl font-bold mb-2 sm:mr-4">Location</label>
                   <div className="w-full sm:w-96">
                     <select
                       id="location"
                       name="location"
-                      placeholder="Enter Location"
-                      autoComplete="off"
                       value={values.location}
                       onChange={handleChange}
                       onBlur={handleBlur}
                       className="block w-full h-9 py-2 px-3 bg-white rounded-md border-0 text-gray-800 ring-1 ring-inset ring-gray-400"
                     >
                       <option value="" disabled>Select location</option>
-                      {/* {batches.map(batch => {
-                        return <option key={batch._id} value={batch._id}>{batch.batchName}</option>;
-                      })} */}
+                      {/* Add your location options here */}
                     </select>
-                    {/* {errors.batch && touched.batch && <p className="text-[#dc2626]">{errors.batch}</p>} */}
+                    {errors.location && touched.location && <p className="text-[#dc2626]">{errors.location}</p>}
                   </div>
                 </div>
 
-                {/* Upload Sheet */}
-                <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-                  <label className="text-gray-700 text-xl font-bold mb-2 sm:mr-4">Upload Sheet</label>
+                {/* File Upload */}
+                <div className="flex flex-col sm:flex-row items-start justify-between">
+                  <label htmlFor="userDetailsFile" className="text-gray-700 text-xl font-bold mb-2 sm:mr-4">Upload Sheet</label>
                   <div className="w-full sm:w-96 flex justify-center rounded-lg border border-dashed border-gray-900/25 p-3">
                     <div className="text-center">
                       <label
@@ -125,13 +110,13 @@ function BulkEntryXlsx() {
                           type="file"
                           name="userDetailsFile"
                           id="userDetailsFile"
-                          value={values.userDetailsFile}
                           onChange={handleChange}
                           onBlur={handleBlur}
                           className="flex h-10 w-full rounded-md border border-input bg-white px-3 py-2 text-sm text-gray-400 file:border-0 file:bg-transparent file:text-gray-600 file:text-sm file:font-medium"
                         />
                       </label>
-                      <p className="text-xs leading-5 text-gray-600">only .xlsx or .xls supported up to 10MB</p>
+                      <p className="text-xs leading-5 text-gray-600">Only .xlsx or .xls supported up to 10MB</p>
+                    {errors.userDetailsFile && touched.userDetailsFile && ( <p className="text-xs text-[#dc2626]">{errors.userDetailsFile}</p> )}
                     </div>
                   </div>
                 </div>
@@ -140,15 +125,17 @@ function BulkEntryXlsx() {
                 <div className="flex justify-center">
                   <button
                     type="submit"
-                    className="text-white bg-[#0A1C3E] hover:text-[#0A1C3E] border border-white hover:bg-white hover:border-[#0A1C3E] focus:ring-4 focus:outline-none focus:ring-[#0A1C3E]-100 font-medium rounded-lg text-sm px-5 py-1.5 text-center m-4"
+                    className="text-white bg-[#0A1C3E] hover:text-[#0A1C3E] border border-white hover:bg-white hover:border-[#0A1C3E] focus:ring-4 focus:outline-none focus:ring-[#0A1C3E]-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-4"
                   >
                     Submit
                   </button>
                   <Link
-                    className="text-white bg-[#0A1C3E] hover:text-[#0A1C3E] border border-white hover:bg-white hover:border-[#0A1C3E] focus:ring-4 focus:outline-none focus:ring-[#0A1C3E]-100 font-medium rounded-lg text-sm px-5 py-1.5 text-center m-4"
-                    to="http://localhost:5000/api/users/download/trainee/input">
-                       Download Sample
-                    </Link>
+                    className="text-white bg-[#0A1C3E] hover:text-[#0A1C3E] border border-white hover:bg-white hover:border-[#0A1C3E] focus:ring-4 focus:outline-none focus:ring-[#0A1C3E]-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-4"
+                    to="http://localhost:5000/api/users/download/trainee/input"
+                  >
+                    Download Sample
+                    <FaDownload className="inline-block ml-2" />
+                  </Link>
                 </div>
 
               </div>
