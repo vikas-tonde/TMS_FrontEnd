ae919c92242418ca4606793a29d568c5ac247ee7
Piyush Jaiswal <thejaiswal1@gmail.com>
2024-12-26 10:55:53 +0530

    updated Exams UI

--- src/Pages/Exams.jsx
+++ src/Pages/Exams.jsx
@@ -78,7 +78,6 @@ function Exams() {
 
   return (
     <>
-
       <div className="flex-1 bg-gray-500 bg-opacity-40 pb-6 backdrop-blur-md min-h-screen">
         <Link
           to='/exams'
@@ -89,47 +88,56 @@ function Exams() {
 
         {/* StatisticsTabsMenu without 3D effect */}
         <div className="mt-4 mx-6 shadow-md rounded-lg p-3 bg-white">
-          <div x-data="{ search: '' }">
-            <div className="my-3 col-span-full flex w items-center justify-center">
-              <label
-                htmlFor="batch"
-                className="block text-xl font-medium h-9 mt-4  text-gray-900 mr-2"
-              >
-                Select batch :
-              </label>
-              <div className="mt-4 pl-10">
-                <select
-                  id="batch"
-                  name="batch"
-                  autoComplete="off"
-                  value={selectedBatch}
-                  onChange={(e) => setSelectedBatch(e.target.value)}
-                  className="block w-96 h-9 rounded-md border-0 py-1.5 text-gray-800 shadow-md ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset  focus:text-gray-800 mr-2 sm:max-w-xs sm:text-sm sm:leading-6"
-                >
-                  <option value="" selected disabled>Select the batch</option>
-                  {batches.map(batch => (
-                    <option key={batch._id} value={batch._id}>{batch.batchName}</option>
-                  ))}
-                </select>
-              </div>
-            </div>
 
-            <div class="mb-2 w-full flex items-center justify-start space-x-2">
-              <svg class="w-5 h-8 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+          <div className="my-3 col-span-full flex items-center justify-center space-x-4">
+            <label
+              htmlFor="batch"
+              className="block text-xl font-medium h-9 text-gray-900 mr-2"
+            >
+              Select batch :
+            </label>
+            <div className="mt-4 sm:mt-0 w-full sm:w-96">
+              <select
+                id="batch"
+                name="batch"
+                autoComplete="off"
+                value={selectedBatch}
+                onChange={(e) => setSelectedBatch(e.target.value)}
+                className="block w-full h-9 rounded-md border-0 py-1.5 text-gray-800 shadow-md ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:text-gray-800 sm:max-w-xs sm:text-sm sm:leading-6"
+              >
+                <option value="" selected disabled>
+                  Select the batch
+                </option>
+                {batches.map(batch => (
+                  <option key={batch._id} value={batch._id}>
+                    {batch.batchName}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
+
+          {/* Search Bar */}
+          <div x-data="{ search: '' }">
+            <div className="mb-2 w-full flex items-center justify-start space-x-2">
+              <svg className="w-5 h-8 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
               </svg>
               <input
                 type="search"
-                onChange={(e) => { setsearch(e.target.value) }}
-                class="h-8 pr-2 py-1 w-full text-gray-800 focus:outline-none"
+                onChange={(e) => { setSearch(e.target.value) }}
+                className="h-8 pr-2 py-1 w-full text-gray-800 focus:outline-none"
                 placeholder="Search Trainee by Id / Name" />
             </div>
+
+            {/* Table */}
             <table className="shadow-sm p-6 h-max w-full text-left mb-5 border-spacing-0" id="table-to-xls">
-              <thead className="bg-blue text-white p-3 h-16 ">
+              <thead className="bg-blue text-white p-3 h-16">
                 {table.getHeaderGroups().map((headerGroup) => (
                   <tr key={headerGroup.id}>
                     {headerGroup.headers.map((header) => (
-                      <th key={header.id} className="capitalize px-4 py-2 ">
+                      <th key={header.id} className="capitalize px-4 py-2">
                         {flexRender(
                           header.column.columnDef.header,
                           header.getContext()
@@ -145,30 +153,18 @@ function Exams() {
                     <tr
                       key={row.i}
                       className={`
-                    ${i % 2 === 0 ? "bg-white" : "bg-white"} border-b border-gray-300 h-16 hover:bg-neutral-200 
-                    `}
+                        ${i % 2 === 0 ? "bg-white" : "bg-white"} border-b border-gray-300 h-16 hover:bg-neutral-200
+                      `}
                     >
-                      <td className="px-4 py-2 ">
-                        {i + 1}
-                      </td>
-                      <td className="px-4 py-2 ">
-                        {row.assessmentName}
-                      </td>
-                      <td className="px-4 py-2 ">
-                        {row.moduleName}
-                      </td>
-                      <td className="px-4 py-2 ">
-                        {row.date}
-                      </td>
-                      <td className="px-4 py-2 ">
-                        {row.totalMarks}
-                      </td>
-                      <td className="px-4 py-2 ">
-                        {row.assessmentType}
-                      </td>
+                      <td className="px-4 py-2">{i + 1}</td>
+                      <td className="px-4 py-2">{row.assessmentName}</td>
+                      <td className="px-4 py-2">{row.moduleName}</td>
+                      <td className="px-4 py-2">{row.date}</td>
+                      <td className="px-4 py-2">{row.totalMarks}</td>
+                      <td className="px-4 py-2">{row.assessmentType}</td>
                       <td key="edit" className="px-4 py-2">
                         <Link to={`/exams/${row._id}`}>
-                          <button className="bg-blue text-white font-bold py-2 px-4 rounded" >
+                          <button className="bg-blue text-white font-bold py-2 px-4 rounded">
                             Details
                           </button>
                         </Link>
@@ -182,14 +178,11 @@ function Exams() {
                 )}
               </tbody>
             </table>
-
           </div>
-
         </div>
       </div>
-
     </>
-  )
+  );
 }
 
-export default Exams
+export default Exams;
