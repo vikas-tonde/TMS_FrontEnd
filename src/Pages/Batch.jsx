38cc5bc08af33e0ef615940b0356620b8efb550e
Piyush Jaiswal <thejaiswal1@gmail.com>
2025-01-02 10:37:28 +0530

    UI updated for Batch

--- src/Pages/Batch.jsx
+++ src/Pages/Batch.jsx
@@ -1,13 +1,22 @@
-import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { useLoaderData } from "react-router";
-const Batch = () => {
 
+const Batch = () => {
   let batch = useLoaderData();
   console.log(batch);
   return (
     <>
-      <div className="flex-1 m-10" >
-        <h3 className="pt-5 pl-5 pr-10 font-bold text-3xl">{batch?.batchName}</h3>
+    <div className="flex-1 bg-gray-500 bg-opacity-40 pb-6 backdrop-blur-md min-h-screen">
+        <Link
+          to='#'
+          className="block py-3 px-3 font-bold text-3xl text-gray-700 Times text-center mx-auto"
+        >
+          {batch?.batchName}
+        </Link>
+
+        <div className="mt-4 mx-6 shadow-xl rounded-lg p-3 bg-white">
+          
+        </div>
       </div>
     </>
   );
